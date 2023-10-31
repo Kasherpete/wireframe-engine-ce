2ee1c10e9da24efd243eda87b77a98151f14362c
Kasherpete <Keagan.a.peterson@outlook.com>
2023-10-30 19:15:01 -0500

    Correct/optimize variable types, add render distance, clean up

--- src/main.c
+++ src/main.c
@@ -1,53 +1,67 @@
-// #include <ti/screen.h>
 #include <keypadc.h>
-#include <stdlib.h>
 #include <graphx.h>
+
 #include <sys/timers.h>
 #include <math.h>
 #include <time.h>
-#include <stdio.h>
+#include <stdint.h>
 
 
-#define FOV 0.9
+
+#define FOV 0.8
 #define LEN(arr) ((int) (sizeof (arr) / sizeof (arr)[0]))
 
 #define GFX_HEIGHT_HALF 120
 #define GFX_WIDTH_HALF 160
 
+// all exclusive
+#define RENDER_DIST_X 4  // dont draw below -4x or higher than 4x
+#define RENDER_DIST_Y 3
+#define RENDER_DIST_Z_BACK 3 // back from the camera
+#define RENDER_DIST_Z_FRONT 8 // above 0
+
+// simple rectangular prism. cone-shape would be best.
+#define RENDER_DISTANCE_ALGORITHM (z > -RENDER_DIST_Z_BACK) && (z < RENDER_DIST_Z_FRONT) && (x < RENDER_DIST_X) && (x > -RENDER_DIST_X) && (y < RENDER_DIST_Y) && (y > -RENDER_DIST_Y)
+
 
 struct Player
 {
-    char x;
-    char y;
-    char z;
+    int_fast8_t x;
+    int_fast8_t y;
+    int_fast8_t z;
 };
 
 
 void drawBox(int x, int y, int z) {
 
-    // just some optimizations
-    const int powAZx50 = pow(FOV,z) * 50;
-    const int powAZp1x50 = pow(FOV,(z+1)) * 50;
+    // render distance calculations. needs improvement
+    if (RENDER_DISTANCE_ALGORITHM) {
 
-    // calculate values
-    int w = (x*powAZx50) + GFX_WIDTH_HALF;
-    int v = (y*powAZx50) + GFX_HEIGHT_HALF;
-    int u = ((x-1)*powAZx50) + GFX_WIDTH_HALF;
-    int t = ((y-1)*powAZx50) + GFX_HEIGHT_HALF;
-    int r = (x*powAZp1x50) + GFX_WIDTH_HALF;
-    int q = (y*powAZp1x50) + GFX_HEIGHT_HALF;
-    int p = ((x-1)*powAZp1x50) + GFX_WIDTH_HALF;
-    int o = ((y-1)*powAZp1x50) + GFX_HEIGHT_HALF;
+        // just some optimizations
+        const uint_fast8_t powAZx50 = pow(FOV,z) * 50;
+        const uint_fast8_t powAZp1x50 = pow(FOV,(z+1)) * 50;
 
-    // front box
-    gfx_Rectangle(u, t, w-u+1, w-u+1);
-    // back box
-    gfx_Rectangle(p,o,r-p+1, r-p+1);
-    // lines
-    gfx_Line(r, q, w, v);
-    gfx_Line(r, o, w, t);
-    gfx_Line(p, o, u, t);
-    gfx_Line(p, q, u, v);
+        // calculate values - data type needs to go from -1 to 241 height, 321 width
+        // potential optimization here. willing to make viewport 255x255
+        int_fast16_t w = (x*powAZx50) + GFX_WIDTH_HALF;
+        int_fast16_t v = (y*powAZx50) + GFX_HEIGHT_HALF;
+        int_fast16_t u = ((x-1)*powAZx50) + GFX_WIDTH_HALF;
+        int_fast16_t t = ((y-1)*powAZx50) + GFX_HEIGHT_HALF;
+        int_fast16_t r = (x*powAZp1x50) + GFX_WIDTH_HALF;
+        int_fast16_t q = (y*powAZp1x50) + GFX_HEIGHT_HALF;
+        int_fast16_t p = ((x-1)*powAZp1x50) + GFX_WIDTH_HALF;
+        int_fast16_t o = ((y-1)*powAZp1x50) + GFX_HEIGHT_HALF;
+
+        // front box
+        gfx_Rectangle(u, t, w-u+1, w-u+1);
+        // back box
+        gfx_Rectangle(p,o,r-p+1, r-p+1);
+        // lines
+        gfx_Line(r, q, w, v);
+        gfx_Line(r, o, w, t);
+        gfx_Line(p, o, u, t);
+        gfx_Line(p, q, u, v);
+    }
 }
 
 
